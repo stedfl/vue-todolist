4dcb066f3c6947b34dbfd41e165a0477f17166c5
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2022-11-04 17:12:20 +0100

    add new task adder function with string text control
    If new task string has a length under 5 or his text is equal to
    the text of another existing task, different messages of error
    appear for the specific case

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ createApp({
       counterDone: 0,
       counterUndone: 0,
       logoImg: {path:'img/logo.png', name: 'logo Boolean' },
+      inputText: '',
       tasks: [
         {
           text: 'Andare a fare la spesa',
@@ -44,7 +45,28 @@ createApp({
       } else {
         this.errorMessage = 'Devi completare il task prima di poterlo rimuovere'
       }
-      
+    },
+    addTask() {
+      let isNew = true;
+      this.errorMessage = '';
+      if (this.inputText.length >= 5) {
+        for (task of this.tasks) {
+          if (this.inputText === task.text) {
+            this.errorMessage = 'Task già esistente';
+            isNew = false;
+          }
+        }
+        if (isNew) {
+          const newTask = {
+            text: this.inputText,
+            done: false
+          }
+          this.tasks.unshift(newTask);
+        }
+      } else {
+        this.errorMessage = 'Inserire un task minimo di 5 caratteri'
+      }
+      this.inputText = ''
     }
 
   },
