cdce429ccd73112023b50b47649686e8b5f9f5d2
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2022-11-04 15:33:10 +0100

    add with vue task list, condition for done class and counter tasks

--- js/script.js
+++ js/script.js
@@ -2,11 +2,34 @@ const {createApp} = Vue;
 createApp({
   data() {
     return {
-      logoImg: {path:'img/logo.png', name: 'logo Boolean' }
-
+      errorMessage: '',
+      logoImg: {path:'img/logo.png', name: 'logo Boolean' },
+      tasks: [
+        {
+          text: 'Andare a fare la spesa',
+          done: false
+        },
+        {
+          text: 'pagare bolletta luce',
+          done: false
+        },
+        {
+          text: 'comprare il regalo per Elisabetta',
+          done: true
+        }
+      ]
     }
   },
   methods: {
+    counterDoneTask() {
+      this.taskCounter = 0;
+      for (task of this.tasks) {
+        if (!task.done) {
+          this.taskCounter++;
+        }
+      }
+      return this.taskCounter
+    }
 
   }
 }).mount("#app");
